4953d716f6f03aba8bdec4c0abfee674260fbec7
Susanne <20076477+susannedevlin@users.noreply.github.com>
2025-07-21 15:04:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -119,6 +119,7 @@ function skipToNext() {
 
 // Playlist switcher
 function switchPlaylist(name) {
+    console.log("Switching to playlist:", name);
   if (!PLAYLISTS[name]) return alert("Playlist not found");
   PLAYLIST_ID = PLAYLISTS[name];
   loadPlaylist();
